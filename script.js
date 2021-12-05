393c6e15b96591dd2fb39205333e276876ea3db3
Joshua Farnaby <joshuafarnabyuk@gmail.com>
2021-12-05 16:42:18 +0000

    cancel add book button will hide modal

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const addBookBtn = document.querySelector('.add-book-button');
-const addBookModal = document.querySelector('.modal-container')
+const addBookModal = document.querySelector('.modal-container');
+const cancelAddBookBtn = document.querySelector('#cancel');
 
 
 function showAddBookModal() {
   addBookModal.style.display = 'flex';
 }
 
+function hideAddBookModal() {
+  // clear all inputs
+  addBookModal.style.display = 'none';
+}
+
 addBookBtn.addEventListener('click', showAddBookModal)
+cancelAddBookBtn.addEventListener('click', hideAddBookModal)
