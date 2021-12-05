f02708010c6879b69eec97ef8e118af44ef764a1
Joshua Farnaby <joshuafarnabyuk@gmail.com>
2021-12-05 16:38:30 +0000

    add book button will show add book modal when clicked

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-const
+const addBookBtn = document.querySelector('.add-book-button');
+const addBookModal = document.querySelector('.modal-container')
+
+
+function showAddBookModal() {
+  addBookModal.style.display = 'flex';
+}
+
+addBookBtn.addEventListener('click', showAddBookModal)
