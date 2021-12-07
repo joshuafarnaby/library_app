d18733d40088dcec8acebe145c1296d052b948e3
Joshua Farnaby <joshuafarnabyuk@gmail.com>
2021-12-07 12:01:27 +0000

    move functions to Book prototype

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const inputFields = document.querySelectorAll('.new-book');
 const cancelAddBookBtn = document.querySelector('#cancel');
 const submitNewBookBtn = document.querySelector('#submit');
 
+const cardContainer = document.querySelector('.card-container');
+
 const books = [];
 
 window.onload = function() {
@@ -12,12 +14,13 @@ window.onload = function() {
   let item;
 
   while(window.localStorage.getItem(index)) {
-    item = window.localStorage.getItem(index);
-    books.push(JSON.parse(item));
+    item = JSON.parse(window.localStorage.getItem(index));
+
+    books.push(new Book(...item));
     index++;
   }
   
-  books.forEach(book => displayBookCard(book))
+  books.forEach(book => book.displayBook())
 }
 
 function Book(title, author, pages, haveRead) {
@@ -27,6 +30,65 @@ function Book(title, author, pages, haveRead) {
   this.haveRead = haveRead;
 }
 
+Book.prototype.createBookHTML = function() {
+  let title = this.title ? this.title : 'unknow';
+  let author = this.author ? this.author : 'uknown';
+  let pages = this.pages ? this.pages : 'unknown';
+  let haveRead = this.haveRead ? 'Read' : 'Unread';
+
+  let dataIndex = books.indexOf(this);
+
+  let newCard = document.createElement('div');
+  newCard.classList.add('card');
+  newCard.setAttribute('data-index', dataIndex);
+
+  newCard.innerHTML = `
+    <div class="card-header">
+      <button class="remove-book-btn">X</button>
+      <h3>${title}</h3>
+    </div>
+    <div class="info-container">
+      <p>Author: ${author}</p>
+      <p>Length: ${pages} pages</p>
+      <div class="has-read-container">
+        <p>${haveRead}</p>
+        <button class="change-read-status">${this.haveRead ? 'unread' : 'read'}</button>
+      </div>
+    </div>`
+
+  return newCard
+}
+
+Book.prototype.displayBook = function() {
+  let bookHTML = this.createBookHTML();
+  cardContainer.appendChild(bookHTML);
+}
+
+Book.prototype.valuesToArray = function() {
+  let valuesArray = [];
+
+  for (let key in this) {
+    valuesArray.push(this[key]);
+  }
+
+  return valuesArray;
+}
+
+Book.prototype.saveToLocalStorage = function() {
+  let arrayOfValues = this.valuesToArray();
+  let indexOfBook = books.indexOf(this);
+  window.localStorage.setItem(indexOfBook, JSON.stringify(arrayOfValues));
+}
+
+Book.prototype.removeFromArray = function(array) {
+  if (array.indexOf(this) == -1) return;
+  array.splice(books.indexOf(this), 1)
+}
+
+Book.prototype.changeReadStatus = function() {
+  this.haveRead = !this.haveRead;
+}
+
 function handleNewBookSubmission(e) {
   e.preventDefault();
 
@@ -34,53 +96,15 @@ function handleNewBookSubmission(e) {
   let newBook = new Book(...inputFieldValues);
 
   books.push(newBook);
-  displayBookCard(newBook);
-  window.localStorage.setItem(books.indexOf(newBook), JSON.stringify(newBook))
+  newBook.displayBook();
+  newBook.saveToLocalStorage();
+
   hideAddBookModal();
 }
 
-function displayBookCard(bookObj) {
-  let newBookCard = generateNewBookCard(bookObj)
-
-  document.querySelector('.card-container').appendChild(newBookCard)
-}
-
-function changeReadStatus(index, textToEdit, buttonToEdit) {
-  let bookToEdit = books[index];
-  bookToEdit.haveRead = !bookToEdit.haveRead;
-  textToEdit.textContent = bookToEdit.haveRead ? 'Read' : 'Unread';
-  buttonToEdit.textContent = bookToEdit.haveRead ? 'unread' : 'read';
-
-  window.localStorage.setItem(index, JSON.stringify(bookToEdit));
-}
-
-function generateNewBookCard(bookObj) {
-  let bookTitle = bookObj.title ? bookObj.title : 'unknown';
-  let bookAuthor = bookObj.author ? bookObj.author : 'unknown';
-  let bookPages = bookObj.pages ? bookObj.pages : 'unknown';
-  let readStatus = bookObj.haveRead ? 'Read' : 'Unread';
-
-  let dataIndex = books.indexOf(bookObj)
-
-  let newCard = document.createElement('div')
-  newCard.classList.add('card');
-  newCard.setAttribute('data-index', dataIndex)
-
-  newCard.innerHTML = `
-    <div class="card-header">
-      <button class="remove-book-btn">X</button>
-      <h3>${bookTitle}</h3>
-    </div>
-    <div class="info-container">
-      <p>Author: ${bookAuthor}</p>
-      <p>Length: ${bookPages} pages</p>
-      <div class="has-read-container">
-        <p>${readStatus}</p>
-        <button class="change-read-status">${bookObj.haveRead ? 'unread' : 'read'}</button>
-      </div>
-    </div>`
-
-  return newCard
+function changeReadStatusOnCard(textToEdit, buttonToEdit) {
+  textToEdit.textContent = textToEdit.textContent == 'Unread' ? 'Read' : 'Unread';
+  buttonToEdit.textContent = buttonToEdit.textContent == 'read' ? 'unread' : 'read';
 }
 
 function updateDataIndexAttributes() {
@@ -123,7 +147,9 @@ function hideAddBookModal(e = null) {
 function updateLocalStorage() {
   window.localStorage.clear();
 
-  books.forEach((book, idx) => window.localStorage.setItem(idx, JSON.stringify(book)));
+  books.forEach((book) => {
+    book.saveToLocalStorage();
+  })
 }
 
 submitNewBookBtn.addEventListener('click', handleNewBookSubmission);
@@ -133,10 +159,13 @@ cancelAddBookBtn.addEventListener('click', hideAddBookModal);
 document.addEventListener('click', (e) => {
   if (e.target && e.target.classList.contains('change-read-status')) {
     let index = e.target.parentElement.parentElement.parentElement.getAttribute('data-index');
+    let bookToEdit = books[index]
     let textToEdit = e.target.previousElementSibling;
     let buttonToEdit = e.target;
 
-    changeReadStatus(index, textToEdit, buttonToEdit);
+    bookToEdit.changeReadStatus();
+    bookToEdit.saveToLocalStorage();
+    changeReadStatusOnCard(textToEdit, buttonToEdit);
   }
 })
 
@@ -144,9 +173,10 @@ document.addEventListener('click', (e) => {
   if (e.target && e.target.classList.contains('remove-book-btn')) {
     let cardToRemove = e.target.parentElement.parentElement;
     let index = cardToRemove.getAttribute('data-index');
+    let targetBook = books[index];
 
-    document.querySelector('.card-container').removeChild(cardToRemove);
-    books.splice(index, 1);
+    cardContainer.removeChild(cardToRemove);
+    targetBook.removeFromArray(books);
 
     updateDataIndexAttributes();
     updateLocalStorage();
